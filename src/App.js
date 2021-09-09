33fad273a12bee0c1057af5b709f0c0c4696f045
tahminahesari <hesari@live.com>
2021-09-09 16:34:17 +0200

    eerste opdracht

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import './components/Buttons.js';
+
 
 function App() {
   return (
+  <>
       <h1>Handbags & Purses</h1>
-  );
+      <nav>
+        <button type="button"></button>
+
+      </nav>
+</>
+  )
 }
 
 export default App;
